c786b89b322c0fcec08833b1a67b9cd91074e4c3
MooMooMod <wilsonianprogrammer@gmail.com>
2022-04-08 21:14:53 +0100

    init

---
+++ plans.ino
@@ -0,0 +1,47 @@
+/*
+
+Overall goals for drone:
+>Lift off
+>Stabilisation
+>Movement
+    -Pre-programmed
+    -Radio control
+    -Autonomous
+>Landing
+
+
+Other considerations:
+>Safety
+    -Prevent crashing
+    -Manual shut down button
+    -Prevent battery dying
+    -Auto return to home
+
+
+Hardware:
+>Microcontroller on drone
+>IMU
+>Radio
+>GPS
+>Camera (possibly a microcomputer i.e rPi)
+    -FPV
+    -Cinematic
+>Surroundings sensor
+    -Lidar / Sonar
+    -Ultrasonic
+    -Infrared
+    -Visible
+
+>Wires
+>PCB
+
+>Motors
+>ESCs
+>Battery
+>Frame
+
+
+
+
+
+*/
